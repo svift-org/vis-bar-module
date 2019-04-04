31a07d0f586f6b0ecf15701e64bb3176720811ba
hhack <hans.f.hack@googlemail.com>
2019-04-04 14:58:16 +0200

    make animation faster

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ SVIFT.vis.barchart = (function (data, container) {
  
   var module = SVIFT.vis.base(data, container);
  
+
+
+  //  var b1 = 4 / 11,
+  //     b2 = 6 / 11,
+  //     b3 = 8 / 11,
+  //     b4 = 3 / 4,
+  //     b5 = 9 / 11,
+  //     b6 = 10 / 11,
+  //     b7 = 15 / 16,
+  //     b8 = 21 / 22,
+  //     b9 = 63 / 64,
+  //     b0 = 1 / b1 / b1;
+
+  // function bounceOut(t) {
+  //   return (t = +t) < b1 ? b0 * t * t : t < b3 ? b0 * (t -= b2) * t + b4 : t < b6 ? b0 * (t -= b5) * t + b7 : b0 * (t -= b8) * t + b9;
+  // }
+
+
   module.d3config = {
-    ease:d3.easeBounceOut, 
+    ease:d3.easeBounce, 
     xInterpolate:[],
     steps:data.data.data.length,
     // animation:{
@@ -36,8 +54,8 @@ SVIFT.vis.barchart = (function (data, container) {
 
 
     module.timeline = {
-      bars: {start:0, end:2500, func:module.barAnimation},
-      text: {start:2500, end:3000, func:module.textAnimation},
+      bars: {start:0, end:1500, func:module.barAnimation},
+      text: {start:1500, end:3000, func:module.textAnimation},
     };
 
 
